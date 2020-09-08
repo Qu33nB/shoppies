301454a1c27c341dfa41d4006c820284ee27bc8e
Qu33nB <bdcotton52@gmail.com>
2020-09-07 23:03:28 -0500

    Added search bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SearchBar from './SearchBar';
 function App() {
   return (
     <div className="App">
+    <SearchBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
