d36f9cce967d844377db860b0b44421f82a16e8b
Qu33nB <bdcotton52@gmail.com>
2020-09-07 22:13:43 -0500

    Updates

---
+++ precache-manifest.0557fe522cc0c02251ca09c6dc6dec3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1003c00687e02cccfd05badd9f624d",
+    "url": "/shoppies/index.html"
+  },
+  {
+    "revision": "a611ce3a39fcf9539e7e",
+    "url": "/shoppies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d42d970c0fb58763894a",
+    "url": "/shoppies/static/js/2.30dce397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppies/static/js/2.30dce397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a611ce3a39fcf9539e7e",
+    "url": "/shoppies/static/js/main.baaddedc.chunk.js"
+  },
+  {
+    "revision": "27778a2488007f8c9380",
+    "url": "/shoppies/static/js/runtime-main.da667ea0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppies/static/media/logo.5d5d9eef.svg"
+  }
+]);
